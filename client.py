7a76ed2e5478ba4dcea3c223035e484055c950a2
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-03 17:30:59 +0200

    commit

--- client.py
+++ client.py
@@ -1,21 +1,86 @@
 import socket
 import threading
 
+host = '127.0.0.1'
 nickname = input("Choose the nickname:")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 50000))
+client.connect((host, 50000))
+not_finish = True
+
+
+def receive_file(port_listen, port_hack):
+    print("GOT INSIDE THE THREAD RECIEVE FILE")
+    client_socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket_udp.bind((host, port_listen))
+    print(port_listen , port_hack)
+    recv_packets = []
+    # index_packet = []
+    # num_of_packets = client_socket_udp.recv(1024).decode()
+    # client_socket_udp.send(b'cool, thanks')
+
+    # while not_finish:
+    #
+    #     tmp_msg = client_socket_udp.recv(1024).decode()
+    #     packet_num = int.from_bytes(tmp_msg[0:3], byteorder='big')
+    #     if packet_num not in index_packet:
+    #         client_socket_udp.settimeout()
+    #         recv_packets.append(tmp_msg[3:len(tmp_msg)])
+    #     client_socket_udp.sendto(str(packet_num).encode(), (host, port_sender))
+    # not_finish = True
+
+    while not_finish:
+        tmp_msg = client_socket_udp.recv(1024).decode()
+        recv_packets.append(tmp_msg)
+        print(recv_packets)
+        client_socket_udp.sendto(str(len(recv_packets) - 1).encode(), (host, port_hack))
+    print("poopopopopop")
+    client_socket_udp.close()
+    f = open("recv_packets.txt", "w")
+    for i in recv_packets:
+        print("LDLDLDLD")
+        f.write(i)
+    f.close()
+
+    # print("poopopopopop")
+    # client_socket_udp.close()
+    # f = open("recv_packets.txt", "w")
+    # for i in recv_packets:
+    #     print("LDLDLDLD")
+    #     f.write(i)
+    # f.close()
+
+
+
+    # file open ...
+    # add the recv packets in to the file
 
 
 def receive():
     while True:
         try:
             message = client.recv(1024).decode()
+            split_msg = message.split('~')
             if message == 'NICK':
                 client.send(nickname.encode())
+            elif split_msg[0] == 'starting udp download':
+                print("GOT INTO DOWNLOAD")
+                listener_sender = split_msg[1].split(',')
+                print(listener_sender)
+                print(listener_sender[0][2:7])
+                print(listener_sender[1][1:6])
+                port_listen = int(listener_sender[0][2:7])  # add substring
+                port_sender = int(listener_sender[1][1:6])
+                print(port_listen , port_sender)
+                recive_file_thread = threading.Thread(target=receive_file, args=[port_listen, port_sender])
+                recive_file_thread.start()
+            elif message == 'Download file done':
+                print("WE FINISHED WITH THIS SHIT")
+                not_finish = False
+
             else:
                 print(message)
         except:
-            print("Error")
+            print("Error - MadaFucka")
             client.close()
             break
 
@@ -29,6 +94,5 @@ def write():
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
-
 write_thread = threading.Thread(target=write)
 write_thread.start()
--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import threading
 import socket
 
+
+
+# download file-billyjoel_music.txt
+
+
 host = '127.0.0.1'
 port = 50000
+byte_size = 500
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(5)
 clients = []
 nicknames = []
+udp_clients = {}
+file_names = []
+buffer_udp = {}
+avilable_port = [0 for i in range(0, 15)]
+socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 
 # The protocol between the users:
@@ -18,8 +30,95 @@ nicknames = []
 # To disconnect - just write 'exit'
 
 
-
 # a function to send a message for all the group
+
+
+def read_line(file_name, index):
+    print("check2")
+    f = open(file_name, "rb")
+    data = f.read(500)
+    buffer_udp[nicknames[index]] = data
+    list_data = []
+    list_data.append(data)
+    while (data):
+        data = f.read(500)
+        list_data.append(data)
+        # if (socket_udp.sendto(data.encode(), (host, tmp_dest))):
+
+
+            # time.sleep(0.02)  # Give receiver a bit time to save
+    # socket_udp.close()
+    f.close()
+    print(list_data)
+    return list_data
+
+
+
+def download_udp(index, file_name):
+    # global port_listen
+    # file_size = os.path.getsize(file_name)
+    # num_of_packet = math.ceil(file_size / byte_size)
+    # if buffer_udp.get(nicknames[index]) is None:
+    #     buffer_udp[nicknames[index]] = []
+    global port_listen
+    flag = True
+    for i in range(0, 15):
+        if flag:
+            if avilable_port[i] == 0:
+                port_listen = i + 55002
+                avilable_port[i] = 1
+                for j in range(i + 1, 15):
+                    if avilable_port[j] == 0:
+                        port_ack = j + 55002
+                        print(i , j)
+                        print(port_listen, port_ack)
+                        avilable_port[j] = 1
+                        flag = False
+                        socket_udp.bind((host, port_ack))
+                        # udp_clients[nicknames[index]] = (port_listen, socket_udp)
+                        clients[index].send(f'starting udp download~ {port_listen, port_ack}'.encode())
+                        print("check1")
+                        break
+    print("check1.5")
+    # print(buffer_udp[nicknames[index]])
+    # print(len(buffer_udp[nicknames[index]]))
+    file_arr = read_line(file_name,index)
+    # print(buffer_udp[nicknames[index]])
+    # print(len(buffer_udp[nicknames[index]]))
+    for i in range(0, len(file_arr)):
+        print(len(file_arr))
+        print("check1.8")
+        got_hack = True
+        print("check4")
+        while got_hack:
+            # packet_num = (i).to_bytes(3, byteorder='big')
+            # print(packet_num)
+            # print(buffer_udp[nicknames[index]][i])
+
+
+            # data = buffer_udp[nicknames[index]][i]
+            # print(data)
+
+            # packet = packet_num + data
+            # if port_listen != -1:
+            print("check5")
+            socket_udp.sendto(file_arr[i], ('127.0.0.1', port_listen))
+            print("check6")
+            try:
+                socket_udp.settimeout(0.02)
+                msg = socket_udp.recv(1024).decode()
+                if int(msg) == i:
+                    print("MESSAGE = I")
+                    got_hack = False
+                    print("GOT HACK = FALSE")
+                    break
+            except:
+                continue
+
+    clients[index].send(f'Download file done'.encode())
+    socket_udp.close()
+
+
 def brodcast(message):
     for client in clients:
         client.send(message)
@@ -30,6 +129,7 @@ def handle(client):
         try:
             message = client.recv(1024).decode()
             index = clients.index(client)
+            split_msg = message.split('-')
             # if the client wants to disconnect
             if message == f'{nicknames[index]}: exit':
                 index = clients.index(client)
@@ -44,18 +144,30 @@ def handle(client):
             elif message == f'{nicknames[index]}: get names':
                 names_message = str(nicknames)
                 clients[index].send(names_message.encode())
+            # if the client wants to get the names of the files in the server
+            elif message == f'{nicknames[index]}: get files':
+                files_message = str(file_names)
+                clients[index].send(files_message.encode())
+            # if the client wants to download a specific file
+            elif split_msg[0] == f'{nicknames[index]}: download file':
+                file_name = split_msg[1]
+                if file_name in file_names:
+                    udp_thread = threading.Thread(target=download_udp, args=(index, file_name))
+                    udp_thread.start()
+                else:
+                    clients[index].send('there is no such a file'.encode())
             # regular message or a private message
             else:
                 count = 0
                 flag = 1
                 for nick in nicknames:
                     # checking if the message is a private message
-                    if str("<"+str(nick)+">") in message:
-                        private_msg = message.replace(str(nicknames[index]) + ": <" +str(nick)+">" , "PRIVATE - <" + str(nicknames[index])+"," + str(nick) + ">")
+                    if str("<" + str(nick) + ">") in message:
+                        private_msg = message.replace(str(nicknames[index]) + ": <" + str(nick) + ">",
+                                                      "PRIVATE - <" + str(nicknames[index]) + "," + str(nick) + ">")
                         clients[count].send(private_msg.encode())
                         clients[index].send(private_msg.encode())
                         flag = 0
-                        print("CHECK")
                     count += 1
                 if flag:
                     brodcast(message.encode())
@@ -89,6 +201,9 @@ def recieve():
         thread.start()
 
 
-print("server is listening...")
-recieve()
-
+if __name__ == '__main__':
+    print("server is listening...")
+    file_names.append("billyjoel_music.txt")
+    file_names.append("classic_music.txt")
+    file_names.append("israeli_music.txt")
+    recieve()
